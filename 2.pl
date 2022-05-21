7ef8dee7b12e0586cee7639a89c7aa6a261a2c29
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-05-21 03:59:50 +0300

    11 задание (рекурсия вниз)
    Исправил рекурсию вверх и добавил рекурсию вниз.

--- 2.pl
+++ 2.pl
@@ -4,10 +4,15 @@ nod(A,B,X):-C is A mod B,nod(B,C,X),!.
 
 even(X):- (X mod 2) =:= 0 .
 coprime(X,Y):- nod(X,Y,Z), Z = 1.
+notcoprime(X,Y):- not(coprime(X,Y)).
 
 % 11 задание 
 % рекурсия вверх 
-count_impl(X,1,CNT):- CNT is 0.
-count_impl(X,Y,CNT):- Y>1, NY is Y-1, count_impl(X,NY,NCNT),(coprime(X,NY),CNT is NCNT;CNT is NCNT+1),!.
+count_impl(_,1,CNT):- CNT is 0.
+count_impl(X,Y,CNT):- Y>1, NY is Y-1, count_impl(X,NY,NCNT),(notcoprime(X,Y),even(Y),CNT is NCNT+1;CNT is NCNT),!.
+countnotprime(X,Y):-NX is X-1,count_impl(X,NX,Y).
 
-countnotprime(X,Y):-count_impl(X,X,Y).
+% рекурсия вниз
+count_down(_,1,CNT,OUT):-OUT is CNT,!.
+count_down(X,Y,CNT,OUT):-Y>1,NY is Y-1,(notcoprime(X,Y),even(Y),NCNT is CNT+1;NCNT is CNT),count_down(X,NY,NCNT,OUT).
+count_down(X,Y):-NX is X-1,count_down(X,NX,0,Y),!.
