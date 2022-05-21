b91c530dfc32bae5eaf9f2d7dafd74777f47b6d1
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-05-21 04:36:32 +0300

    12 задание

--- 2.pl
+++ 2.pl
@@ -16,3 +16,19 @@ countnotprime(X,Y):-NX is X-1,count_impl(X,NX,Y).
 count_down(_,1,CNT,OUT):-OUT is CNT,!.
 count_down(X,Y,CNT,OUT):-Y>1,NY is Y-1,(notcoprime(X,Y),even(Y),NCNT is CNT+1;NCNT is CNT),count_down(X,NY,NCNT,OUT).
 count_down(X,Y):-NX is X-1,count_down(X,NX,0,Y),!.
+
+% 12 задание
+mindel(X,X,Z):-Z is X,!.
+mindel(X,Y,Z):- X mod Y =:= 0,Z is Y;Y1 is Y + 1,mindel(X,Y1,Z).
+mindel(X,Y):-mindel(X,2,Y),!.
+
+maxnotcoprime(_,1,_,Z):-Z is 1,!.
+maxnotcoprime(X,Y,D,Z):-Y mod D =\= 0,notcoprime(X,Y),Z is Y;Y1 is Y-1,maxnotcoprime(X,Y1,D,Z).
+maxnotcoprime(X,Y):-X1 is X-1,mindel(X,D),maxnotcoprime(X,X1,D,Y),!.
+
+numbsum(0,SUM,OUT):-OUT is SUM,!.
+numbsum(X,SUM,OUT):-X > 0,NewX is X div 10, DIGIT is X mod 10,(DIGIT < 5,NSUM is SUM + DIGIT;NSUM is SUM),numbsum(NewX,NSUM,OUT). 
+numbsum(X,Y):-numbsum(X,0,Y),!.
+
+pred(X,Y):-maxnotcoprime(X,D),numbsum(X,A),Y is D*A.
+
