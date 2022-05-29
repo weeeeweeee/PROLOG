42af6d609e63e56c8869a8798b16e9472d29c04d
weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
2022-05-29 18:19:27 +0300

    1.3 задание

--- 4.pl
+++ 4.pl
@@ -7,7 +7,7 @@ r_str(S,N,K,TS,X):-NewK is K + 1,append(TS,[X],NewTS),get0(NewX),r_str(S,N,NewK,
 write_str([]):-nl.
 write_str([H|T]):-put(H),write_str(T).
 
-task1:-read_str(S,N),write_str(S),write_str(S),write_str(S),write("Symbol count:"),write(N).
+task1_1:-read_str(S,N),write_str(S),write_str(S),write_str(S),write("Symbol count:"),write(N).
 
 
 % 1.2 задание
@@ -23,4 +23,29 @@ count_words([_|T],N,true,V):-count_words(T,N,true,V).
 count_words([32|T],N):-count_words(T,N).
 count_words([_|T],N):-count_words(T,1,true,N).
 
-task2:-read_str(S,_),count_words(S,N),write(N),nl.
+task1_2:-read_str(S,_),count_words(S,N),write(N),nl.
+
+
+
+% 1.3 задание
+in_list_exclude([H|T],H,T).
+in_list_exclude([H|T],El,[H|Tail]):-in_list_exclude(T,El,Tail).
+
+get_words([],[[]],_).
+get_words([32|T],Tail,0):-get_words(T,Tail,0),!.
+get_words([32|T],[[]|Tail],_):-get_words(T,Tail,0),!.
+get_words([H|T],[[H|T2]|Tail],N):-N1 is N+1,get_words(T,[T2|Tail],N1),!.
+get_words(List,Out):-get_words(List,Out,0).
+
+
+count_item(List,El,C,V,Out):-in_list_exclude(List,El,List1),!,(C1 is C+1,count_item(List1,El,C1,V,Out));Out = List,V is C.
+count_item(List,El,V,Out):-count_item(List,El,0,V,Out),!.
+
+count_list([],[]).
+count_list([H|T],[H2|T2]):-count_item([H|T],H,V,L2),H2 = [H,V],count_list(L2,T2).
+
+find_max([],_,Item,Item).
+find_max([[H1,H2]|T],Max,Item,Out):-(Max < H2,Max1 is H2,Item1 = H1;Max1 is Max,Item1 = Item),find_max(T,Max1,Item1,Out).
+find_max([[H1,H2]|T],Out):-find_max(T,H2,H1,Out),!.
+
+task1_3:-read_str(S,_),get_words(S,S1),count_list(S1,Out),find_max(Out,MAX),write_str(MAX).
